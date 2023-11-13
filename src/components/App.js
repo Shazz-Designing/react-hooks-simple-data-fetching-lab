5a5d337409a2610848db8fe12d3aab798341bf20
sharon <sapot@safaricom.co.ke>
2023-11-13 22:04:54 +0300

    Update app.js to display random dogs

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,38 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch data from the Dog API
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Update state with the fetched image URL
+        setDogImage(data.message);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching dog image:', error);
+        setLoading(false); 
+      });
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" style={{ maxWidth: '100%' }} />
+      )}
+    </div>
+  );
+}
+
+export default App;
